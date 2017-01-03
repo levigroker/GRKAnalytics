26a104e7ecc31af5cc1b0676bcbf3bcc59a10a0b
Levi Brown <levigroker@gmail.com>
2017-01-03 11:46:51 -0700

    Updating test project with new build proceedure.

--- GRKAnalyticsTestApp/Pods/Target Support Files/GRKAnalytics/GRKAnalytics-umbrella.h
+++ GRKAnalyticsTestApp/Pods/Target Support Files/GRKAnalytics/GRKAnalytics-umbrella.h
@@ -5,8 +5,6 @@
 #import "GRKAnalytics.h"
 #import "GRKAnalyticsProvider.h"
 #import "GRKLanguageFeatures.h"
-#import "GRKFabricProvider.h"
-#import "GRKGoogleAnalyticsProvider.h"
 
 FOUNDATION_EXPORT double GRKAnalyticsVersionNumber;
 FOUNDATION_EXPORT const unsigned char GRKAnalyticsVersionString[];
