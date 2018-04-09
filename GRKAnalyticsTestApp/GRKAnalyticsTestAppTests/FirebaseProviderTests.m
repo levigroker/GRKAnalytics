678b13eae61461aa17e6a0dbd934c768599102a1
Levi Brown <levigroker@gmail.com>
2018-04-09 11:02:42 -0600

    Flesh out cropString tests

--- GRKAnalyticsTestApp/GRKAnalyticsTestAppTests/FirebaseProviderTests.m
+++ GRKAnalyticsTestApp/GRKAnalyticsTestAppTests/FirebaseProviderTests.m
@@ -38,35 +38,116 @@
 
 - (void)testCropString100 {
 	
-	NSString *inString = @"";
+	NSString *inString = nil;
 	NSUInteger maxLength = 0;
 	
 	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
 	
-	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
-	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString == nil, @"Unexpectedly did not recieve nil output.");
 }
 
 - (void)testCropString200 {
 	
-	NSString *inString = @"a";
+	NSString *inString = @"";
 	NSUInteger maxLength = 0;
-	
-	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	NSUInteger expectedLength = 0;
 
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
 	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
 	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
 }
 
 - (void)testCropString300 {
 	
-	NSString *inString = @"abcdeefghijklmnop";
-	NSUInteger maxLength = 1;
-	
+	NSString *inString = @"a";
+	NSUInteger maxLength = 0;
+	NSUInteger expectedLength = 0;
+
 	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
 
 	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
 	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString400 {
+	
+	NSString *inString = @"";
+	NSUInteger maxLength = 1;
+	NSUInteger expectedLength = 0;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString500 {
+	
+	NSString *inString = @"a";
+	NSUInteger maxLength = 1;
+	NSUInteger expectedLength = maxLength;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString600 {
+	
+	NSString *inString = @"abcdeefghijklmnop";
+	NSUInteger maxLength = 1;
+	NSUInteger expectedLength = maxLength;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString700 {
+	
+	NSString *inString = @"abcdeefghijklmnop";
+	NSUInteger maxLength = inString.length - 1;
+	NSUInteger expectedLength = maxLength;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString800 {
+	
+	NSString *inString = @"abcdeefghijklmnop";
+	NSUInteger maxLength = inString.length;
+	NSUInteger expectedLength = maxLength;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
+}
+
+- (void)testCropString900 {
+	
+	NSString *inString = @"abcdeefghijklmnop";
+	NSUInteger maxLength = inString.length + 1;
+	NSUInteger expectedLength = inString.length;
+
+	NSString *outString = [self.provider cropString:inString maxLength:maxLength];
+	
+	XCTAssertTrue(outString != nil, @"Unexpectedly recieved nil output.");
+	XCTAssertTrue(outString.length <= maxLength, @"outString.length (%d) unexpectedly greater than maxLength (%d).", (int)outString.length, (int)maxLength);
+	XCTAssertTrue(outString.length == expectedLength, @"outString.length (%d) unexpectedly not equal to expectedLength (%d).", (int)outString.length, (int)expectedLength);
 }
 
 @end
